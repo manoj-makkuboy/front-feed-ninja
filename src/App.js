a3451c237d5c8e60a8cbb51caf64d48a393f41ba
Manoj Mohan <m.manoj@linuxmail.org>
2017-09-01 09:09:48 +0530

    standard js

--- src/App.js
+++ src/App.js
@@ -2,86 +2,106 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-let sample_article = [
+let sample_article = []
+
+	/* [
 	{"title": "title_value", "description": "description"},
 	{"title": "title_value1", "description": "description"},
-{"title": "title_value", "description": "description"},
+	{"title": "title_value", "description": "description"},
 	{"title": "title_value1", "description": "description"}
-         ]
+         ] */
 
+async function getArticles () {
+  try {
+    let response = await fetch('http://localhost:8000/feed_ninja/articles?page=1', { mode: 'no-cors'})
+    let responseJson = await response.json()
+    console.log('heelo', responseJson)
+    this.setState({articles: JSON.parse(responseJson.json()) })
+    return JSON.parse(responseJson.json())
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
 
 class App extends Component {
-  constructor() {
-  	super();
-	this.state = {articles : sample_article,
-			currentPage : 1,
-			articlesPerPage : 3}
-	this.handleClick = this.handleClick.bind(this)
+  constructor () {
+    super()
+    this.state = {
+      articles: sample_article,
+      currentPage: 1,
+      articlesPerPage: 3
+    }
+    this.handleClick = this.handleClick.bind(this)
   }
-  handleClick(e){
+  handleClick (e) {
     this.setState({currentPage: Number(e.target.id)})
   }
 
-  render () {
-   const articles = this.state.articles
-   const currentPage = this.state.currentPage
-   const articlesPerPage = this.state.articlesPerPage
+  componentDidMount () {
+    getArticles.bind(this)
+    this.setState({articles: getArticles() })
+  }
 
-   const indexOfLastPage = currentPage * articlesPerPage
-   const indexOfFirstPage = indexOfLastPage - articlesPerPage
-	  
-   const currentArticles = articles.slice(indexOfFirstPage, indexOfLastPage)
-   
-   const renderedArticles = currentArticles.map((article, index) => {return (
-	   <div key={index}>
-	<li>
-	   <ul>
-	   <li >{article.title}</li>
-  	<li> {article.description}</li> 
-	   </ul>
-	   </li>
-  	</div>									 
-   									);})
+  render () {
+    const articles = this.state.articles
+    const currentPage = this.state.currentPage
+    const articlesPerPage = this.state.articlesPerPage
+
+    const indexOfLastPage = currentPage * articlesPerPage
+    const indexOfFirstPage = indexOfLastPage - articlesPerPage
+
+    const currentArticles = articles.slice(indexOfFirstPage, indexOfLastPage)
+
+    const renderedArticles = currentArticles.map((article, index) => {
+      return (
+        <div key={index}>
+          <li>
+            <ul>
+              <li >{article.title}</li>
+              <li> {article.description}</li>
+            </ul>
+          </li>
+        </div>
+   									)
+    })
 
    // Logic for displaying page numbers
 
-   const pageNumbers = []
-   for (let i = 1; i <= Math.ceil(articles.length / articlesPerPage); i++){
+    const pageNumbers = []
+    for (let i = 1; i <= Math.ceil(articles.length / articlesPerPage); i++) {
    	pageNumbers.push(i)
-   }
-  
+    }
 
-   const renderedPageNumbers = pageNumbers.map(number => { return (
-	   							<li key={number} id={number} onClick={this.handleClick}>
-   								{number}
-   								</li>);}
+    const renderedPageNumbers = pageNumbers.map(number => {
+      return (
+        <li key={number} id={number} onClick={this.handleClick}>
+          {number}
+        </li>)
+    }
 						   )
-   return (<div>
-	     <ul>
-	   	{renderedArticles}
-	     </ul>
-	     
-	   	{renderedPageNumbers}
-	    
-	   </div>
-   );
+    return (<div>
+      <ul>
+        {renderedArticles}
+      </ul>
 
+      {renderedPageNumbers}
 
-}
+    </div>
+    )
+  }
 }
 
 class Articles extends Component {
   render () {
     return (<div>
-	    	<ol>
-		    <li> {sample_article[this.props.id]['title']} </li>
-		    <li> {sample_article[this.props.id]['author']} </li>
-	    	</ol>
-	    </div>
+      <ol>
+        <li> {sample_article[this.props.id]['title']} </li>
+        <li> {sample_article[this.props.id]['author']} </li>
+      </ol>
+    </div>
     )
-
   }
 }
-
 
 export default App
