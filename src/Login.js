19f0e65b9318ee8e829331bcb54dd12281b5f5ed
Manoj Mohan <m.manoj@linuxmail.org>
2017-09-04 16:40:35 +0530

    added actual login and feature to store return jwt in localstorage

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react'
 
+async function sendUserCredentials (bindedSetState, username, password) {
+  let init = {
+    mode: 'cors',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({'username': username, 'password': password})
+  }
+  try {
+    let response = await fetch('http://localhost:8000/rest-auth/login/', init)
+    let jwtToken = await response.json()
+    jwtToken = jwtToken['token']
+    bindedSetState({ jwt: jwtToken })
+    localStorage.setItem('JWT', jwtToken)
+  } catch (error) {
+    console.error(error)
+  }
+}
 class Login extends Component {
   constructor () {
     super()
-    this.state = { username: '', password: '' }
+    this.state = {username: '', password: '', jwt: ''}
 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
   }
 
   handleSubmit (event) {
-    console.log(this.state.username)
+    console.log(this.state)
     event.preventDefault()
+    sendUserCredentials(this.setState.bind(this), this.state.username, this.state.password)
   }
 
   handleInputChange (event) {
-    this.setState
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value
+    })
+    console.log(name)
   }
 
   render () {
@@ -22,13 +50,13 @@ class Login extends Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           username :
-          <input type='text' name='username' />
+          <input type='text' onChange={this.handleInputChange} value={this.state.username} name='username' />
         </label>
         <br />
         <br />
         <label>
           password :
-          <input type='password' name='password' />
+          <input type='password' name='password' onChange={this.handleInputChange} value={this.state.password} />
         </label>
         <br />
         <input type='submit' value='Submit' />
