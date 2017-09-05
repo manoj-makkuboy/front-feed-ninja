cf8ef7ba21ab98836873cbf9b1df7b4fa51c1047
Manoj Mohan <m.manoj@linuxmail.org>
2017-09-05 10:48:39 +0530

    removed unwanted code from App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import ArticleGrid from './ArticleGrid.js'
 import Login from './Login.js'
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = { isLoggedIn: false }
-  /*  this.state = {
-      articles: [],
-      currentPage: 1,
-      articlesPerPage: 3
-    } */
-  }
-
   render () {
-    if (this.state.isLoggedIn) {
-      return (
-        <ArticleGrid />
-      )
-    } else {
-      return (
-        <Login />
-      )
-    }
+    return (<Login />)
   }
 }
 
